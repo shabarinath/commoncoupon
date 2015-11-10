8c300f5235a1dd387d2fd7f3a94b28f45c5ef477
shabarinath <volamshabarinath@gmail.com>
2015-11-10 17:19:56 +0530

    changes in sql file

--- commoncoupon.sql
+++ commoncoupon.sql
@@ -15,9 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---Database creation
-
 CREATE DATABASE IF NOT EXISTS commoncoupon;
+
+USE commoncoupon;
 
 --
 -- Table structure for table `user_roles`
