454d2719c2fc788484c8174d9574bee0bc391331
shabarinath <volamshabarinath@gmail.com>
2015-11-15 00:47:11 +0530

    New POJO for payment response

---
+++ src/main/java/com/commoncoupon/domain/PaymentRequest.java
@@ -0,0 +1,152 @@
+package com.commoncoupon.domain;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class PaymentRequest {
+	
+	@JsonProperty("id")
+	private String id;
+	@JsonProperty("phone")
+	private String phone;
+	@JsonProperty("email")
+	private String email;
+	@JsonProperty("buyer_name")
+	private String buyerName;
+	@JsonProperty("amount")
+	private String amount;
+	@JsonProperty("purpose")
+	private String purpose;
+	@JsonProperty("status")
+	private String status;
+	@JsonProperty("send_sms")
+	private String sendSms;
+	@JsonProperty("send_email")
+	private String SendEmail;
+	@JsonProperty("sms_status")
+	private String smsStatus;
+	@JsonProperty("email_status")
+	private String emailStatus;
+	@JsonProperty("shorturl")
+	private String shortUrl;
+	@JsonProperty("longurl")
+	private String longUrl;
+	@JsonProperty("redirect_url")
+	private String redirectUrl;
+	@JsonProperty("webhook")
+	private String webhook;
+	@JsonProperty("created_at")
+	private String createdAt;
+	@JsonProperty("modified_at")
+	private String modifiedAt;
+	@JsonProperty("allow_repeated_payments")
+	private String allowRepeatedPayments;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBuyerName() {
+		return buyerName;
+	}
+	public void setBuyerName(String buyerName) {
+		this.buyerName = buyerName;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getPurpose() {
+		return purpose;
+	}
+	public void setPurpose(String purpose) {
+		this.purpose = purpose;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getSendSms() {
+		return sendSms;
+	}
+	public void setSendSms(String sendSms) {
+		this.sendSms = sendSms;
+	}
+	public String getSendEmail() {
+		return SendEmail;
+	}
+	public void setSendEmail(String sendEmail) {
+		SendEmail = sendEmail;
+	}
+	public String getSmsStatus() {
+		return smsStatus;
+	}
+	public void setSmsStatus(String smsStatus) {
+		this.smsStatus = smsStatus;
+	}
+	public String getEmailStatus() {
+		return emailStatus;
+	}
+	public void setEmailStatus(String emailStatus) {
+		this.emailStatus = emailStatus;
+	}
+	public String getShortUrl() {
+		return shortUrl;
+	}
+	public void setShortUrl(String shortUrl) {
+		this.shortUrl = shortUrl;
+	}
+	public String getLongUrl() {
+		return longUrl;
+	}
+	public void setLongUrl(String longUrl) {
+		this.longUrl = longUrl;
+	}
+	public String getRedirectUrl() {
+		return redirectUrl;
+	}
+	public void setRedirectUrl(String redirectUrl) {
+		this.redirectUrl = redirectUrl;
+	}
+	public String getWebhook() {
+		return webhook;
+	}
+	public void setWebhook(String webhook) {
+		this.webhook = webhook;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getModifiedAt() {
+		return modifiedAt;
+	}
+	public void setModifiedAt(String modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+	public String getAllowRepeatedPayments() {
+		return allowRepeatedPayments;
+	}
+	public void setAllowRepeatedPayments(String allowRepeatedPayments) {
+		this.allowRepeatedPayments = allowRepeatedPayments;
+	}
+}
---
+++ src/main/java/com/commoncoupon/domain/PaymentResponse.java
@@ -0,0 +1,30 @@
+package com.commoncoupon.domain;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PaymentResponse {
+	@JsonProperty("payment_request")
+	private PaymentRequest paymentRequest;
+	
+	@JsonProperty("success")
+	private String isSuccess;
+
+	public PaymentRequest getPaymentRequest() {
+		return paymentRequest;
+	}
+
+	public void setPaymentRequest(PaymentRequest paymentRequest) {
+		this.paymentRequest = paymentRequest;
+	}
+
+	public String getIsSuccess() {
+		return isSuccess;
+	}
+
+	public void setIsSuccess(String isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+}
